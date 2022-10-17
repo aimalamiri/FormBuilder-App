be783f9d50f869d72b3551342dcfda06634f5dac
Aimal Amiri <meaimal@hotmail.com>
2022-10-17 23:14:56 +0430

    Add a container div

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,9 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <>
       <Navbar />
-      <Component {...pageProps} />
+      <div className="container pt-5 mx-auto px-8 sm:px-5 lg:px-12">
+        <Component {...pageProps} />
+      </div>
   </>
 }
 
