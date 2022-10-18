33ed4580c4da20a4593fcf991e872fed4a266dfe
Aimal Amiri <meaimal@hotmail.com>
2022-10-18 19:06:32 +0430

    Add login email and password fields

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,8 +1,19 @@
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
-      <p>Not yet implemented</p>
+      <div className="card w-1/3 mx-auto">
+        <h1 className="text-xl text-center mb-9">Login</h1>
+        <form action="">
+          <label for="email" className="label">
+            Email
+            <input type="email" placeholder="Enter your email" className="input" />
+          </label>
+          <label for="password" className="label my-4">
+            Password
+            <input type="password" placeholder="Enter your password" className="input" />
+          </label>
+        </form>
+      </div>
     </>
   )
 }
