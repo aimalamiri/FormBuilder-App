3f7ea0edfaa448cfd8edbd965b452e9efb70c8fd
Aimal Amiri <meaimal@hotmail.com>
2022-10-25 19:09:30 +0430

    Add credentials state to login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+import axios from '../../config/axios';
+
 export default function Login() {
+  const data = {email: '', password: ''};
+  const [credentials, setCredentials] = useState(data);
+
+  const submit = (e) => {
+    e.preventDefault();
+    axios.post('/login', credentials).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const inputHandler = (e) => {
+    setCredentials({...credentials, [e.target.name]: e.target.value});
+  };
+
+  const {email, password} = credentials;
+
   return (
     <>
       <div className="card w-1/3 mx-auto">
         <h1 className="text-xl text-center mb-9">Login</h1>
-        <form action="">
-          <label for="email" className="label">
+        <form onSubmit={submit}>
+          <label htmlFor="email" className="label">
             Email
-            <input type="email" placeholder="Enter your email" className="input" />
+            <input type="email" name="email" placeholder="Enter your email" className="input" value={email} onChange={inputHandler} />
           </label>
-          <label for="password" className="label my-4">
+          <label htmlFor="password" className="label my-4">
             Password
-            <input type="password" placeholder="Enter your password" className="input" />
+            <input type="password" name="password" placeholder="Enter your password" className="input" value={password} onChange={inputHandler} />
           </label>
           <button className="btn btn-success" type="submit">Submit</button>
         </form>
