843162b3793bc66b551bab39a03020994f602f00
Aimal Amiri <meaimal@hotmail.com>
2022-10-30 23:43:40 +0430

    Add logout component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
+import useAuth from '../utils/useAuth';
 
 export default function Navbar() {
+  const [auth] = useAuth();
   return (
     <nav className="w-100 py-4 bg-white">
       <ul className="flex justify-center items-center gap-4 text-gray-700 hover:opacity-90 text-lg">
         <li><Link href="/">Home</Link></li>
         <li><Link href="/projects">Projects</Link></li>
         <li><Link href="/forms">Forms</Link></li>
-        <li className="absolute right-8 sm:right-5 lg:right-12"><Link href="/auth/login">Login</Link></li>
+        <li className="absolute right-8 sm:right-5 lg:right-12">
+          {auth.user.first_name ? (
+            <Link href="/auth/logout">Logout</Link>
+          ) : (
+            <Link href="/auth/login">Login</Link>
+          )}
+        </li>
       </ul>
     </nav>
   );
---
+++ pages/auth/logout.js
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../redux/reducers/auth/authSlice';
+
+export default function Logout() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.removeItem('jwt');
+      localStorage.removeItem('user');
+      router.push('/');
+      dispatch(login({user: {}, jwt: ''}));
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Logout</h1>
+    </div>
+  );
+}
