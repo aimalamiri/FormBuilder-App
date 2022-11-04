7b2b1197e7e46b83dcf3587b184203eb8b6030fe
Aimal Amiri <meaimal@hotmail.com>
2022-11-04 19:04:12 +0430

    Design listing projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,3 +1,4 @@
+import ProjectCard from '../../components/projects/ProjectCard';
 import useAuth from '../../utils/useAuth';
 
 export default function Projects() {
@@ -5,7 +6,10 @@ export default function Projects() {
 
   return (
     <div>
-      <h1>Projects</h1>
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-2">
+        <ProjectCard project={{name: 'project 1', description: 'project 1 description'}} />
+      </div>
     </div>
   );
 }
