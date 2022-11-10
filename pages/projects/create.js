126fcd4f4bd3003720de235677daee928e321176
Aimal Amiri <meaimal@hotmail.com>
2022-11-10 18:10:49 +0430

    Design the projects form

---
+++ pages/projects/create.js
@@ -0,0 +1,39 @@
+import Input from '../../components/application/input';
+import TextArea from '../../components/application/textarea';
+
+import { useState } from 'react';
+
+export default function Create() {
+  const data = { name: '', description: '' };
+  const [project, setProject] = useState(data);
+  const { name, description } = project;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProject({ ...project, [name]: value });
+  };
+
+  return (
+    <div className="card w-1/3 mx-auto">
+      <h1 className="text-xl text-center mb-9">Create Project</h1>
+      <form>
+        <Input
+          name="name"
+          placeholder="Enter project name"
+          value={name}
+          className="mb-4"
+          onChange={handleChange}
+        />
+        <TextArea
+          name="description"
+          placeholder="Enter project description"
+          value={description}
+          onChange={handleChange}
+        />
+        <div className="text-end">
+          <button className="btn btn-success mt-4">Create</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { getProjects } from '../../api/projects';
 import ProjectCard from '../../components/projects/ProjectCard';
@@ -16,6 +17,7 @@ export default function Projects() {
   return (
     <div>
       <h1 className="text-3xl font-semibold">Projects</h1>
+      <Link href="/projects/create"><button className="btn btn-success mt-4">Create Project</button></Link>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-2">
         {projects.length > 0 ? (
           projects.map((project) => (
