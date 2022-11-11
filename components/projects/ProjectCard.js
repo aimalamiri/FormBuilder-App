250d882dc3061dbf393785833d4ce2401d8bcc1c
Aimal Amiri <meaimal@hotmail.com>
2022-11-11 23:09:03 +0430

    Remove the project card when it's deleted

--- components/projects/ProjectCard.js
+++ components/projects/ProjectCard.js
@@ -7,6 +7,7 @@ import { deleteProject, updateProject } from '../../api/projects';
 export default function ProjectCard({ project }) {
   const [editable, setEditable] = useState(true);
   const [data, setData] = useState(project);
+  const [deleted, setDeleted] = useState(false);
 
   const edit = () => {
     setEditable(!editable);
@@ -46,43 +47,47 @@ export default function ProjectCard({ project }) {
           text: 'The project has been deleted!',
           icon: 'success',
         });
+        setDeleted(true);
       }
     });
   };
 
-  return (
-    <div className="p-5 bg-white shadow rounded hover:shadow-lg">
-      <ContentEditable
-        className="text-xl font-bold mb-4 focus:outline-none w-full"
-        html={data.name}
-        disabled={editable}
-        onChange={changeName}
-      />
-      <ContentEditable
-        html={data.description}
-        disabled={editable}
-        onChange={changeDescription}
-        className="focus:outline-none w-full"
-      />
-      <div className="flex justify-end items-end mt-2 gap-3">
-        {editable ? (
-          <button onClick={edit}>
-            <PencilIcon className="h-4 w-4 text-gray-400 hover:text-gray-600" />
-          </button>
-        ) : (
-          <>
-            <button onClick={update}>
-              <CheckIcon className="h-5 w-5 text-green-700 hover:text-green-600" />
+  if (!deleted) {
+    return (
+      <div className="p-5 bg-white shadow rounded hover:shadow-lg">
+        <ContentEditable
+          className="text-xl font-bold mb-4 focus:outline-none w-full"
+          html={data.name}
+          disabled={editable}
+          onChange={changeName}
+        />
+        <ContentEditable
+          html={data.description}
+          disabled={editable}
+          onChange={changeDescription}
+          className="focus:outline-none w-full"
+        />
+        <div className="flex justify-end items-end mt-2 gap-3">
+          {editable ? (
+            <button onClick={edit}>
+              <PencilIcon className="h-4 w-4 text-gray-400 hover:text-gray-600" />
             </button>
-            <button onClick={remove}>
-              <TrashIcon className="h-5 w-5 text-red-600 hover:text-red-500" />
-            </button>
-            <button onClick={discard}>
-              <NoSymbolIcon className="h-5 w-5 text-gray-400 hover:text-gray-600" />
-            </button>
-          </>
-        )}
+          ) : (
+            <>
+              <button onClick={update}>
+                <CheckIcon className="h-5 w-5 text-green-700 hover:text-green-600" />
+              </button>
+              <button onClick={remove}>
+                <TrashIcon className="h-5 w-5 text-red-600 hover:text-red-500" />
+              </button>
+              <button onClick={discard}>
+                <NoSymbolIcon className="h-5 w-5 text-gray-400 hover:text-gray-600" />
+              </button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return null;
 }
