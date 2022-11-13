b00c6bc5d0bd0092b32972de1c97efb2d3f6ca9f
Aimal Amiri <meaimal@hotmail.com>
2022-11-13 18:07:59 +0430

    Extract all input type buttons to an array of json

--- pages/forms/create.js
+++ pages/forms/create.js
@@ -1,15 +1,38 @@
+import Input from '../../components/application/textarea';
+
 export default function create() {
+  const types = [
+    {
+      type: 'input',
+      title: 'Textbox',
+    },
+    {
+      type: 'textarea',
+      title: 'Textarea',
+    },
+    {
+      type: 'checkbox',
+      title: 'Checkbox',
+    },
+    {
+      type: 'radio',
+      title: 'Radio',
+    },
+    {
+      type: 'select',
+      title: 'Select',
+    },
+  ];
+
   return (
     <div>
       <h1 className="text-3xl font-semibold">Create a new form</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-2">
         <div className="card">
           <h2 className="text-lg">Inputs</h2>
-          <button className="btn btn-gray w-full mt-3">Input</button>
-          <button className="btn btn-gray w-full mt-3">Checkbox</button>
-          <button className="btn btn-gray w-full mt-3">Textarea</button>
-          <button className="btn btn-gray w-full mt-3">Radio</button>
-          <button className="btn btn-gray w-full mt-3">Select</button>
+          {types.map((type) => (
+            <button className="btn btn-gray w-full mt-3" key={type.type}>{type.title}</button>
+          ))}
         </div>
         <div className="card col-span-2">
           <h1>View</h1>
@@ -19,5 +42,5 @@ export default function create() {
         </div>
       </div>
     </div>
-  )
+  );
 }
