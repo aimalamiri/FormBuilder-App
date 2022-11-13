6e2fef0452f8a089ed4168e303045ebb55ced5a0
Aimal Amiri <meaimal@hotmail.com>
2022-11-13 17:59:05 +0430

    Add input type button to forms/create

--- pages/forms/create.js
+++ pages/forms/create.js
@@ -4,7 +4,12 @@ export default function create() {
       <h1 className="text-3xl font-semibold">Create a new form</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5 gap-2">
         <div className="card">
-          <h1>Inputs</h1>
+          <h2 className="text-lg">Inputs</h2>
+          <button className="btn btn-gray w-full mt-3">Input</button>
+          <button className="btn btn-gray w-full mt-3">Checkbox</button>
+          <button className="btn btn-gray w-full mt-3">Textarea</button>
+          <button className="btn btn-gray w-full mt-3">Radio</button>
+          <button className="btn btn-gray w-full mt-3">Select</button>
         </div>
         <div className="card col-span-2">
           <h1>View</h1>
