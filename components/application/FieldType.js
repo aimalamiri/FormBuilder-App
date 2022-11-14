516f64106fc44540e118376f49863d0b9c945d17
Aimal Amiri <meaimal@hotmail.com>
2022-11-14 16:24:05 +0430

    Add FieldType component

---
+++ components/application/FieldType.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function FieldType({ types, onChange }) {
+  console.log(types);
+  return (
+    <div className="flex justify-between">
+      <label className="text-sm p-3">{types[0]}</label>
+      <select className="input" onChange={onChange}>
+        {Object.entries(types[1]).map((type) => (
+          <option value={type[0]} key={type[0]}>
+            {type[0]}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
