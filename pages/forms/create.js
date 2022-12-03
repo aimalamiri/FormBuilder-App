982ac28354a4adcb95dae482588f2c7c1b2bee21
Aimal Amiri <meaimal@hotmail.com>
2022-12-03 21:51:09 +0430

    Design the form name and description form layout

--- pages/forms/create.js
+++ pages/forms/create.js
@@ -5,6 +5,7 @@ import Property from '../../components/application/Property';
 
 export default function Create() {
   const [inputs, setInputs] = useState([]);
+  const [form, setForm] = useState({ name: '', description: '' });
   const [activeField, setActiveField] = useState({});
 
   const addInput = (field) => {
@@ -12,53 +13,69 @@ export default function Create() {
   };
 
   const fields = [
-  {
-    tagname: 'input',
-    clsname: 'input',
-    properties: {
-      title: 'Textbox',
-      type: { text: true, number: false, email: false, password: false },
-      placeholder: '',
+    {
+      tagname: 'input',
+      clsname: 'input',
+      properties: {
+        title: 'Textbox',
+        type: { text: true, number: false, email: false, password: false },
+        placeholder: '',
+      },
     },
-  },
-  {
-    tagname: 'textarea',
-    clsname: 'input',
-    properties: {
-      title: 'Textarea',
-      placeholder: '',
+    {
+      tagname: 'textarea',
+      clsname: 'input',
+      properties: {
+        title: 'Textarea',
+        placeholder: '',
+      },
     },
-  },
-  {
-    tagname: 'checkbox',
-    clsname: 'input',
-    properties: {
-      title: 'Checkbox',
-      options: ['Option'],
+    {
+      tagname: 'checkbox',
+      clsname: 'input',
+      properties: {
+        title: 'Checkbox',
+        options: ['Option'],
+      },
     },
-  },
-  {
-    tagname: 'radio',
-    clsname: 'input',
-    properties: {
-      title: 'Radio',
-      options: ['Option'],
+    {
+      tagname: 'radio',
+      clsname: 'input',
+      properties: {
+        title: 'Radio',
+        options: ['Option'],
+      },
     },
-  },
-  {
-    tagname: 'select',
-    clsname: 'input',
-    properties: {
-      title: 'Select',
-      placeholder: 'Select an option',
-      options: [],
+    {
+      tagname: 'select',
+      clsname: 'input',
+      properties: {
+        title: 'Select',
+        placeholder: 'Select an option',
+        options: [],
+      },
     },
-  },
-];
+  ];
 
   return (
     <div className="min-h-[90vh]">
       <h1 className="text-3xl font-semibold">Create a new form</h1>
+      <div className="card grid grid-cols-2">
+        <label className="w-2/4 flex justify-end items-center gap-2" htmlFor="name">
+          <span className="flex justify-end">Name:</span>
+          <input type="text" name="name" value={form.name} onChange={() => {}} className="input" />
+        </label>
+        <label className="w-2/4 flex justify-start items-center gap-2" htmlFor="name">
+          Description:
+          <textarea
+            type="text"
+            name="description"
+            value={form.description}
+            onChange={() => {}}
+            className="input w-full"
+          />
+        </label>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-5 gap-2">
         <div className="card">
           <h2 className="text-lg">Inputs</h2>
