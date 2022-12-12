9611140d44f9db3df4486e465ca68dc64cafdabb
Aimal Amiri <meaimal@hotmail.com>
2022-12-12 16:17:48 +0430

    Add form to the form entry page

--- pages/entry/[id].js
+++ pages/entry/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { getForm } from '../../api/entries';
 import { useState } from 'react';
+import InputField from '../../components/application/InputField';
 
 export default function Entry() {
   const router = useRouter();
@@ -25,9 +26,21 @@ export default function Entry() {
       <div className="card text-center">
         <h1 className="text-5xl">Welcome!</h1>
         <h2 className="text-2xl my-2">Begin your entry by clicking on the button bellow</h2>
-        <button className="btn btn-success my-10" onClick={start}>
-          Start
-        </button>
+        {form.length > 0 ? (
+          <div className="flex justify-center">
+            <div className="w-1/3">
+              {form.map((field) => (
+                <div className="my-3 text-left" key={field.id}>
+                  <InputField input={field} />
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <button className="btn btn-success my-10" onClick={start}>
+            Start
+          </button>
+        )}
       </div>
     </div>
   );
